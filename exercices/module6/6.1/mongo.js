2e2f8664a4f38094de9530eb6f1fffa95dc523d5
KaayTx <kawtar.dahman@student.vinci.be>
2024-12-28 16:53:51 +0100

    6.1

--- exercices/module6/6.1/mongo.js
+++ exercices/module6/6.1/mongo.js
@@ -1,61 +1,41 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
 }
 
 const password = process.argv[2];
-const command = process.argv[3];
 const url = `mongodb+srv://dahmankawtaar:${password}@kawtar07.kbkqp.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Kawtar07`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-}, { collection: 'people' });
-
-const addressSchema = new mongoose.Schema({
-    street: String,
-    city: String,
-}, { collection: 'address' });
+  name: String,
+  number: String,
+});
 
 const Person = mongoose.model('Person', personSchema);
-const Address = mongoose.model('Address', addressSchema);
 
-if (command === 'list') {
-    Person.find({}).then(result => {
-        console.log('phonebook:');
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        });
-        mongoose.connection.close();
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
     });
-} else if (command === 'add-person') {
-    const person = new Person({
-        name: process.argv[4],
-        number: process.argv[5],
-    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
 
-    person.save().then(() => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`);
-        mongoose.connection.close();
-    });
-} else if (command === 'add-address') {
-    const address = new Address({
-        street: process.argv[4],
-        city: process.argv[5],
-    });
-
-    address.save().then(() => {
-        console.log(`added address ${address.street}, ${address.city} to address book`);
-        mongoose.connection.close();
-    });
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
 } else {
-    console.log('Please provide the correct arguments:');
-    console.log('To list all entries: node mongo.js <password> list');
-    console.log('To add a person: node mongo.js <password> add-person <name> <number>');
-    console.log('To add an address: node mongo.js <password> add-address <street> <city>');
-    process.exit(1);
+  console.log('Please provide the correct arguments: node mongo.js <password> <name> <number>');
+  mongoose.connection.close();
 }
